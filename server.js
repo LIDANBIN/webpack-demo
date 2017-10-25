4c645c1a44f88f7d8b5438eb6425034634b2c5ed
LIDANBIN <straberry2@outlook.com>
2017-10-25 16:59:53 +0800

    ''

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const webpack = require('webpack')
+const opn = require('opn')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require("webpack-hot-middleware")
+
+const app = express();
+const config = require('./webpack.config.js')
+const compiler = webpack(config)
+
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
+var devMiddleware = webpackDevMiddleware(compiler, {
+    reload: true, // 自动加载页面
+    quiet: true, // 禁止所有控制台输出
+    hot: true,
+    // target: 'electron-renderer',
+    publicPath: config.output.publicPath
+})
+app.use(devMiddleware)
+app.use(webpackHotMiddleware(compiler))
+
+// Serve the files on port 3000.
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!\n')
+})
+
+// 启动项目后自动运行浏览器
+devMiddleware.waitUntilValid(() => {
+    opn('http://localhost:3000/')
+})
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash'
+import './style.css' // 引入样式表
+import MyImage from './webpack.svg' // 引入图片，图像将被处理并添加到 output 目录。
+
+import Print from './print'
+
+Print()
+
+function component() {
+    // console.log(MyImage); // MyImage 变量将包含该图像在处理后的最终 url（替换后的url）。
+    let element = document.createElement('div')
+    element.innerHTML = _.join(['hello', 'webpack'], ' ')
+
+    // 添加图片
+    let img = new Image()
+    img.classList.add('pic')
+    img.src = MyImage // loader 会识别这是一个本地文件，并将 './my-image.png' 路径，替换为输出目录中图像的最终路径。
+    element.appendChild(img)
+    return element
+}
+
+document.body.appendChild(component())
+
+// if (module.hot) {
+//     module.hot.accept('./print.js', function () {
+//         console.log('Accepting the updated printMe module!');
+//         printMe();
+//     })
+// }
