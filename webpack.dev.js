6b80e389a2b683745d51bf51bc1029a2abd2afa1
LIDANBIN <straberry2@outlook.com>
2017-11-03 16:19:53 +0800

    ''

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,18 @@ const merge = require('webpack-merge')
 const webpack = require('webpack')
 const common = require('./webpack.common.js')
 
+Object.keys(common.entry).forEach(function (name) {
+    common.entry[name] = ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000'].concat(common.entry[name])
+})
+
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     plugins: [
+        new webpack.HotModuleReplacementPlugin(), // 模块热替换webpack-dev-middleware
+        // Use NoErrorsPlugin for webpack 1.x
+        new webpack.NoEmitOnErrorsPlugin(),
         new webpack.DefinePlugin({
+            __DEV__: true,
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
